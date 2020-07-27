d83c373398bb452e82a8f4afd7561fe08af0c865
meaghan jennings <meaghanljennings@gmail.com>
2020-07-27 23:31:28 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,9 +1,8 @@
 def my_find(collection)
   i = 0
   while i < collection.length
-  if yield(collection[i])
-    return collection[i]
-  end
+    return collection[i] if 
+  yield(collection[i])
     i += 1
   end
 end
